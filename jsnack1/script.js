d94482aa521fc77bd05a954ca77f3a1af2e6ac65
Davide Venturelli <dventurelli90@gmail.com>
2022-03-13 14:09:33 +0100

    start

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,7 @@
 //debug
 console.log('JS OK!')
+
+/*
+   Crea un array di numeri interi e fai la somma di tutti gli elementi 
+   che sono in posizione dispari.
+*/
--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,8 @@
 //debug
 console.log('JS OK!')
+
+/*
+   Crea due array che hanno un numero di elementi diversi. 
+   Aggiungi elementi casuali all’array che ha meno elementi,
+    fino a quando ne avrà tanti quanti l’altro.
+*/
